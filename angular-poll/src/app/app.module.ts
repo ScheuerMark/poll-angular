a657486360debcf35cda6fbb0d03c57553f1ae3e
Scheuer Mark <markscheuer2002@gmail.com>
2023-01-18 20:55:23 +0100

    Create Angular project

---
+++ angular-poll/src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { NewPollBtnComponent } from './new-poll-btn/new-poll-btn.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NewPollFormComponent } from './new-poll-form/new-poll-form.component'
+
+const routes: Routes = [
+  { path: 'home',
+    children: [
+        {
+          path:'',
+          component: NewPollBtnComponent
+        },
+        {
+          path:'new-poll',
+          component: NewPollFormComponent
+        }
+    ] ,
+    component:HomeComponent
+  },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NewPollBtnComponent,
+    HomeComponent,
+    NewPollFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
